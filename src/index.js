4eba02d19b88010bfc88107259f8d491de9aadaf
Karimov Islom <126048446+Karimovvv@users.noreply.github.com>
2023-08-13 20:02:50 +0900

    React hooks

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App.jsx';
-import Fruits from './fruits';
+import React from 'react'
+import ReactDOM  from 'react-dom'
+import './Styled.css'
+import App from './App'
 
-// const students =[
-//   {img:1, name:'Apple'},
-//   {img:2, name:'Strawberry'},
-//   {img:3, name:'Lemon'},
-//   {img:4, name:'Watermelon'},
-//   {img:5, name:'Melon'},
-//   {img:6, name:'DragonFruit'}
-// ]
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <Fruits/>
-  </React.StrictMode>
-);
- // students.map((value) =>{
-      //   return (
-      //     <App data = {value}/>
-      //   )
-      // })
-
-
+ReactDOM.render(<App/>, document.getElementById("root"))
